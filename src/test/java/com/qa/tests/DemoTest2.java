1b0850decdfff917b818b79b933da785acdbec66
Virajaji <502001390@medpro.com>
2019-07-08 05:52:26 -0400

    test comments

---
+++ src/test/java/com/qa/tests/DemoTest2.java
@@ -0,0 +1,33 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void Add()
+	{
+		System.out.println("ADD");
+		
+	}
+	
+	@Test
+	public void Substract()
+	{
+		System.out.println("SUBSTRACT");
+		
+	}
+	
+	@Test
+	public void Divide()
+	{
+		System.out.println("Divide");
+		
+	}
+	@Test
+	public void Multiply()
+	{
+		System.out.println("Multiply");
+		
+	}
+}
